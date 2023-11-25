800d7ee465cc11b4d4184d5d27f0afd601f6f60d
Thalisson <thalissonpop91@gmail.com>
2023-11-25 16:28:23 -0300

    GET product paged

--- src/main/java/com/devthalisson/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devthalisson/dscommerce/controllers/ProductController.java
@@ -1,16 +1,14 @@
 package com.devthalisson.dscommerce.controllers;
 
 import com.devthalisson.dscommerce.dto.ProductDTO;
-import com.devthalisson.dscommerce.entities.Product;
-import com.devthalisson.dscommerce.repositories.ProductRepository;
 import com.devthalisson.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -22,6 +20,11 @@ public class ProductController {
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id) {
         return service.findByID(id);
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 
 }
--- src/main/java/com/devthalisson/dscommerce/services/ProductService.java
+++ src/main/java/com/devthalisson/dscommerce/services/ProductService.java
@@ -4,6 +4,8 @@ import com.devthalisson.dscommerce.dto.ProductDTO;
 import com.devthalisson.dscommerce.entities.Product;
 import com.devthalisson.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,5 +23,11 @@ public class ProductService {
         Product product = result.get();
         ProductDTO dto = new ProductDTO(product);
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
     }
 }
