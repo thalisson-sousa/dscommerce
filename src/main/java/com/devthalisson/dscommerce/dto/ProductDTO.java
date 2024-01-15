f73369393796be35f1a79532bc3d0796b43bc524
thalissonsousa <thalissonsousa33@gmail.com>
2024-01-15 00:24:01 -0300

    FIX validation

--- src/main/java/com/devthalisson/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devthalisson/dscommerce/dto/ProductDTO.java
@@ -2,10 +2,7 @@ package com.devthalisson.dscommerce.dto;
 
 import com.devthalisson.dscommerce.entities.Category;
 import com.devthalisson.dscommerce.entities.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +15,7 @@ public class ProductDTO {
     @Size(min = 10, message = "Descrição precisa ter no minimo 10 caracteres")
     @NotBlank(message = "Campo requerido")
     private String description;
+    @NotNull(message = "Campo requerido")
     @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
